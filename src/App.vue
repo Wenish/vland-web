2efd34d1f773978362e1f5d6ab61488049e9b8c1
Wenish <Wenish@users.noreply.github.com>
2021-06-27 21:55:50 +0200

    add reset css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<div>
+  App
+</div>
 </template>
 
 <script lang="ts">
@@ -16,12 +17,12 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
+html, body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #ece8e1;
 }
 </style>
